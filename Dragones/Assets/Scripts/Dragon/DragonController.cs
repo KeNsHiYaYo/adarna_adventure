01af62efb1a4bb526fac1c51ba498de4c112337f
KeNsHiYaYo <ezzeyayo@gmail.com>
2025-04-23 12:09:42 -0600

    Anillos y buff del dragon xd

--- Dragones/Assets/Scripts/Dragon/DragonController.cs
+++ Dragones/Assets/Scripts/Dragon/DragonController.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class DragonController : MonoBehaviour
 {
-    public float moveSpeed = 10f;
-    public float rotationSpeed = 70f;
-    public float ascendSpeed = 5f;
+    public float moveSpeed = 10f; //Velocidad Normal
+    public float boostSpeed = 20f; //Velocidad rapida
+    private float currentSpeed; //Variable donde guardara la velocidad
+
 
     void Update()
     {
-        // Movimiento hacia adelante
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
-
-        // Rotación horizontal
-        float horizontal = Input.GetAxis("Horizontal");
-        transform.Rotate(0f, horizontal * rotationSpeed * Time.deltaTime, 0f);
-
-        // Subir / bajar
-        float vertical = Input.GetAxis("Vertical");
-        transform.position += transform.up * vertical * ascendSpeed * Time.deltaTime;
-
-        // Inclinación visual (opcional)
-        // Puedes inclinar el dragón al girar si quieres más realismo
+        if (Input.GetMouseButton(0)) // Clic izquierdo
+        {
+            currentSpeed = boostSpeed;
+            transform.position += transform.forward * currentSpeed * Time.deltaTime;
+        }
+        else
+        {
+            currentSpeed = moveSpeed;
+            transform.position += transform.forward * currentSpeed * Time.deltaTime;
+        }
     }
 }
 
